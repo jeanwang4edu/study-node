e804a9dc9314aa69871adacb53c8254b6a5ccaff
Jean <jeanharmsen@yahoo.com>
2018-11-12 21:59:35 +0000

    read content from data.txt and output - Part 2

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 //load the http module
-var http = require('http');
+var http = require('http'),
+    fs   = require('fs');
 
 //configure the http server
 http.createServer( (req, res) => {
+
+    //read the content of file data.txt and output to server
+    fs.readFile('data.txt', function readData(err,data) {
 	res.writeHead(200, {'Content-Type':'text/html'});
-        res.end('<h1>Hello World!</h1>');
+        res.end(data);
+    });
+
 }).listen(3000, '0.0.0.0');
 
 console.log("Server running at http://localhost:3000");
